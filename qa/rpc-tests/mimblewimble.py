5d49c441cea1746f43773cd355cca5d94b7d7aaf
tdudz <tdudz97@gmail.com>
2017-07-06 18:20:56 -0700

    fixed test for cutthrough

--- qa/rpc-tests/mimblewimble.py
+++ qa/rpc-tests/mimblewimble.py
@@ -79,7 +79,7 @@ class MimblewimbleTest(BitcoinTestFramework):
         node2change = self.nodes[0].validateaddress(self.nodes[2].getnewaddress())['unconfidential']
   
         # build first incomplete tx, node 2 sending to node 1
-        inputs = [{"txid": txid0, "vout": vout['n'], "amount": Decimal('1.5')}]
+        inputs = [{"txid": txid0, "vout": vout['n']}]
         outputs = {"fee": Decimal('0.05'), node2change: Decimal('0.4')}
         rawtx1 = self.nodes[2].createrawtransaction(inputs, outputs, 0, None, True)
 
@@ -122,20 +122,55 @@ class MimblewimbleTest(BitcoinTestFramework):
         # Now let's test a simple tx with cutthrough 
         ##############################################################################################
 
-        print("")
-        # print(self.nodes[1].listunspent(0, 9999999, []))
-
+        # print("")
+        # print(self.nodes[2].listunspent(0, 9999999, []))
+        print("STARTING CUTTHROUGH TEST")
         node1address = self.nodes[1].validateaddress(self.nodes[1].getnewaddress())['unconfidential']
         node2change = self.nodes[2].validateaddress(self.nodes[2].getnewaddress())['unconfidential']
         node0address = self.nodes[0].validateaddress(self.nodes[0].getnewaddress())['unconfidential']
 
-        inputs = []
+        utxos = self.nodes[2].listunspent(0, 9999999, [])
+        n = float('inf')
+        for utxo in utxos:
+            if utxo["amount"] == Decimal('0.4'):
+                n = utxo["vout"]
+
+        assert(n < float('inf'))
+
+        inputs = [{"txid": mergedtxid, "vout": n}]
         outputs = {"fee": Decimal('0.025'), node1address: Decimal("0.25")}
         rawtx1 = self.nodes[2].createrawtransaction(inputs, outputs, 0, None, True)
 
-        inputs = []
-        outputs = {"fee": Decimal('0.025'), node2change: Decimal('0.01'), node0address: Decimal("0.25")}
+        txid1 = self.nodes[0].decoderawtransaction(rawtx1)['txid']
 
+        reused_n = float('inf')
+        for vout in self.nodes[0].decoderawtransaction(rawtx1)['vout']:
+            if vout['value'] == Decimal('0.25'):
+                reused_n = vout['n']
+
+        assert(reused_n < float('inf'))
+
+        inputs = [{"txid": txid1, "vout": reused_n}]
+        outputs = {"fee": Decimal('0.025'), node2change: Decimal('0.1'), node0address: Decimal("0.25")}
+        rawtx2 = self.nodes[1].createrawtransaction(inputs, outputs, 0, None, True)
+
+        merged = self.nodes[0].mergemwtransactions([rawtx1, rawtx2])
+        jsonmerged = self.nodes[1].decoderawtransaction(merged)
+
+        print("")
+        
+        for thing in jsonmerged['vin']:
+            print(thing)
+            print("")
+        print("--------------------------------------------------")
+        for thing in jsonmerged['vout']:
+            print(thing)
+            print("")
+        
+        merged = self.nodes[2].blindrawtransaction(merged)
+        merged = self.nodes[2].signrawtransaction(merged)['hex']
+        mergedtxid = self.nodes[2].sendrawtransaction(merged)
+        print(mergedtxid)
 
 if __name__ == '__main__':
     MimblewimbleTest().main()
